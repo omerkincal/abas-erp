6add8a80ec56a77e2de5a542622dba644534080e
ÖMER KINCAL <202804010@ogr.cbu.edu.tr>
2024-01-23 13:02:51 +0300

    first commit

---
+++ src/main/java/org/omarkincal/abaserpinterview/AbasErpInterviewApplication.java
@@ -0,0 +1,13 @@
+package org.omarkincal.abaserpinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbasErpInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbasErpInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/omarkincal/abaserpinterview/AbasErpInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omarkincal.abaserpinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbasErpInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
